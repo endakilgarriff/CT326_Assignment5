340701d202903f614cbc9f33cd59dcc0be5881c5
Enda Kilgarriff <enda.kilgarriff@gmail.com>
2019-10-21 08:44:45 +0100

    Implemented hashtable in test class, printing out inventory

--- src/ct326_assignment5/Inventory.java
+++ src/ct326_assignment5/Inventory.java
@@ -1,6 +1,7 @@
 package ct326_assignment5;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 
 public class Inventory {
@@ -9,22 +10,33 @@ public class Inventory {
 	private int quantity;
 	private double price;
 //	private List<Inventory> inventory;
+	Hashtable<String, Object> inventory = new Hashtable<String, Object>();
 	
 	public Inventory(String sku, String itemName, int quantity, double price) {
 		this.sku = sku;
 		this.itemName = itemName;
 		this.quantity = quantity;
 		this.price = price;
-//		System.out.println("Object: " + this);
-//		System.out.println(" List Size: " + inventory.size());
-//		inventory.add(inventory.size() ,this);
 	}
 	
+	public void addToInventory() {
+		inventory.put(this.getSKU(), this);
+	}
+	
+	public String getSKU() {
+		return sku;
+	}
 	
 	@Override
 	public String toString() {
-		return sku + "\t" + itemName + "\t\t" + quantity + "\t" + price + "\n";
+		return sku + "\t" + itemName + "\t" + quantity + "\t" + price;
 			
-	}	
+	}
+	
+	public String hashTablePrint() {
+		return inventory.toString();
+	}
+	
+	
 
 }
--- src/ct326_assignment5/Test.java
+++ src/ct326_assignment5/Test.java
@@ -1,27 +1,56 @@
 package ct326_assignment5;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Test {
+
 	public static void main(String[] args) {
-		
-		List<Inventory> inventory = null;
+
+//		List<Inventory> inventory;
+		Hashtable<String, Object> inventory = new Hashtable<String, Object>();
 //		Inventory inventory;
-		Inventory i1 = new Inventory("1000", "Apple", 30, 2.50);
-		Inventory i2 = new Inventory("1001", "Orange", 40, 2);
-		Inventory i3 = new Inventory("2001", "Milk", 10, 2.39);
-		Inventory i4 = new Inventory("2002", "Orange Juice", 20, 1.99);
-		Inventory i5 = new Inventory("3001", "Blue Cheese", 10, 2.25);
-		Inventory i6 = new Inventory("3002", "Cheddar", 20, 2.79);
-		Inventory i7 = new Inventory("4001", "Chocolate", 40, 2.99);
-		Inventory i8 = new Inventory("4002", "Candy", 30, 0.99);
-		Inventory i9 = new Inventory("5001", "Beef", 10, 5.00);
-		Inventory i10 = new Inventory("5002", "Chicken", 10, 4.00);
+//		inventory = new Inventory("1000", "Apple", 30, 2.50);
+//		inventory = new Inventory("1001", "Orange", 40, 2);
+//		inventory = new Inventory("2001", "Milk", 10, 2.39);
+//		inventory = new Inventory("2002", "Orange Juice", 20, 1.99);
+//		inventory = new Inventory("3001", "Blue Cheese", 10, 2.25);
+//		inventory = new Inventory("3002", "Cheddar", 20, 2.79);
+//		inventory = new Inventory("4001", "Chocolate", 40, 2.99);
+//		inventory = new Inventory("4002", "Candy", 30, 0.99);
+//		inventory = new Inventory("5001", "Beef", 10, 5.00);
+//		inventory = new Inventory("5002", "Chicken", 10, 4.00);
+
+		Inventory inventory1 	= new Inventory("1000", "Apple\t", 30, 2.50);
+		Inventory inventory2 	= new Inventory("1001", "Orange\t", 40, 2);
+		Inventory inventory3 	= new Inventory("2001", "Milk\t", 10, 2.39);
+		Inventory inventory4 	= new Inventory("2002", "Orange Juice", 20, 1.99);
+		Inventory inventory5 	= new Inventory("3001", "Blue Cheese", 10, 2.25);
+		Inventory inventory6 	= new Inventory("3002", "Cheddar\t", 20, 2.79);
+		Inventory inventory7 	= new Inventory("4001", "Chocolate", 40, 2.99);
+		Inventory inventory8 	= new Inventory("4002", "Candy\t", 30, 0.99);
+		Inventory inventory9 	= new Inventory("5001", "Beef\t", 10, 5.00);
+		Inventory inventory10	= new Inventory("5002", "Chicken\t", 10, 4.00);
+
+		inventory.put(inventory1.getSKU(), 	inventory1);
+		inventory.put(inventory2.getSKU(), 	inventory2);
+		inventory.put(inventory3.getSKU(), 	inventory3);
+		inventory.put(inventory4.getSKU(), 	inventory4);
+		inventory.put(inventory5.getSKU(), 	inventory5);
+		inventory.put(inventory6.getSKU(), 	inventory6);
+		inventory.put(inventory7.getSKU(), 	inventory7);
+		inventory.put(inventory8.getSKU(), 	inventory8);
+		inventory.put(inventory9.getSKU(), 	inventory9);
+		inventory.put(inventory10.getSKU(), inventory10);
+
+		Enumeration<String> items;
+		Object key;
 		
-		inventory.add(inventory.size(), i1);
-		
-		
+		items = inventory.keys();
+		   while(items.hasMoreElements()) {
+			      key = items.nextElement();
+			      System.out.println(inventory.get(key));
+			   }
+		   
 //		 new Inventory("1000", "Apple", 30, 2.50);
 //		 new Inventory("1001", "Orange", 40, 2);
 //		 new Inventory("2001", "Milk", 10, 2.39);
@@ -32,8 +61,9 @@ public class Test {
 //		 new Inventory("4002", "Candy", 30, 0.99);
 //		 new Inventory("5001", "Beef", 10, 5.00);
 //		 new Inventory("5002", "Chicken", 10, 4.00);
-		 
-		System.out.println(inventory.toString());
+
+//		System.out.println(inventory.toString());
+
 	}
 
 }
